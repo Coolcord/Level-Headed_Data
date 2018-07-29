09338b6302506eaa01afa3f7c288ae1920e0942e
Coolcord <coolcord24@tutanota.com>
2018-07-29 03:55:26 -0700

    Updated Bouncy Spiny Eggs code

--- ASM/Bouncy Spiny Eggs.asm
+++ ASM/Bouncy Spiny Eggs.asm
@@ -4,15 +4,18 @@ jmp 0x40FD               ; [4CEDC0]
 
 
 0x40FD
-bne #$03 (ldy Enemy_ID,x); [D003]
-jmp $E0E2                ; Jump absolute [4CE2E0]
-ldy Enemy_ID,x           ; [B416]
-cpy #Spiny               ;if enemy object is spiny, branch [C012]
-bne #$03 (jmp 0x600A)    ; [D003]
-jmp 0x5FE9               ; [4CD9DF]
-jmp 0x600A               ; Jump absolute [4CFADF]
+bne #$03 (skip jmp)             ; [D003]
+jmp $E0E2                       ; Jump absolute [4CE2E0]
+ldy Enemy_ID,x                  ; [B416]
+cpy #Spiny                      ; if enemy object is spiny, branch [C012]
+bne #$03 (skip jmp)             ; [D003]
+jmp 0x5FE9                      ; [4CD9DF]
+jsr $E1B5                       ; [20AEE1]
+beq -$11 (to jmp $E0E2)         ; [F0EF]
+jmp 0x600F                      ; Jump absolute [4CFFDF]
 
 
 0x5FE9 = Where bounce code should point to
 0x6007 = A jump to $E0E2. You may just want to jump directly there
-0x600A = Normal code. Send non-spiny eggs here.
+0x600F = Normal code. Send non-spiny eggs here.
+
