37756a611e6904a1e48eb94b34c2d002fbfdc37e
Coolcord <coolcord24@tutanota.com>
2019-09-28 13:18:46 -0700

    Renamed SMB1 levels to a more verbose name

--- Level_Scripts/Compile SMB1.bat
+++ Level_Scripts/Compile SMB1.bat
@@ -2,7 +2,7 @@
 COLOR 0A
 C:
 CD "\Program Files (x86)\Coolcord\SAM"
-SAM.exe --pack "F:\Documents\Source_Code\Level-Headed_Data\Level_Scripts\SMB1"
-RENAME "F:\Documents\Source_Code\Level-Headed_Data\Level_Scripts\SMB1.sa" SMB1.lvls
-DELETE "F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_12_2_MinGW_32_bit-Debug\debug\Levels\SMB1\SMB1.lvls"
-MOVE "F:\Documents\Source_Code\Level-Headed_Data\Level_Scripts\SMB1.lvls" "F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_12_2_MinGW_32_bit-Debug\debug\Levels\SMB1\SMB1.lvls"
+SAM.exe --pack "F:\Documents\Source_Code\Level-Headed_Data\Level_Scripts\Super Mario Bros. 1 (Original Levels without Castle Loops)"
+RENAME "F:\Documents\Source_Code\Level-Headed_Data\Level_Scripts\Super Mario Bros. 1 (Original Levels without Castle Loops).sa" "Super Mario Bros. 1 (Original Levels without Castle Loops).lvls"
+DELETE "F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_12_2_MinGW_32_bit-Debug\debug\Levels\SMB1\Super Mario Bros. 1 (Original Levels without Castle Loops).lvls"
+MOVE "F:\Documents\Source_Code\Level-Headed_Data\Level_Scripts\Super Mario Bros. 1 (Original Levels without Castle Loops).lvls" "F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_12_2_MinGW_32_bit-Debug\debug\Levels\SMB1\Super Mario Bros. 1 (Original Levels without Castle Loops).lvls"
