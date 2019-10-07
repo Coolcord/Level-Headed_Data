a91e6e5cb56a9812c84b30aa5494b581ac931e13
Coolcord <coolcord24@tutanota.com>
2019-10-07 02:20:56 -0700

    The graphics ripper will now automatically generate the original game patch

--- Tools/Graphics_Ripper/main.cpp
+++ Tools/Graphics_Ripper/main.cpp
@@ -7,15 +7,46 @@
 #include <QDir>
 #include <QFileInfo>
 #include <QPluginLoader>
+#include <QTextStream>
+
+//Arguments
+const static QString STRING_GRAPHICS_PATCHES_LOCATION = "F:/Documents/Source_Code/Level-Headed_Data/Graphics";
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
     QString applicationLocation = QCoreApplication::applicationDirPath();
     if (!QFileInfo(applicationLocation+"/SMB1.nes").exists())  { qWarning() << "SMB1.nes does not exist!"; return 1; }
 
-    //Scan for .hexp files to extract graphics from
+    //Copy all patches over to the patches directory
+    qInfo() << "Preparing patches...";
+    QDir patchDir(STRING_GRAPHICS_PATCHES_LOCATION);
     QDir dir(applicationLocation);
     dir.mkdir("Patches");
+    if (patchDir.exists()) {
+        QDir(applicationLocation+"/Patches").removeRecursively();
+        dir.mkdir("Patches");
+        for (QString fileName : patchDir.entryList(QStringList("*.hexp"), QDir::Files | QDir::NoDotAndDotDot)) {
+            QFile file(STRING_GRAPHICS_PATCHES_LOCATION+"/"+fileName);
+            if (!file.open(QIODevice::ReadOnly)) continue;
+            QFile outputFile(applicationLocation+"/Patches/"+fileName);
+            if (!outputFile.open(QIODevice::ReadWrite)) { file.close(); continue; }
+            outputFile.write(file.readAll());
+            file.close();
+            outputFile.close();
+        }
+    }
+
+    //Generate the original patch file
+    QFile originalPatchFile(applicationLocation+"/Patches/Original (by Nintendo.hexp");
+    originalPatchFile.remove();
+    if (!originalPatchFile.open(QIODevice::ReadWrite)) { qWarning() << "Unable to generate the original patch file!"; return 1; }
+    QTextStream originalPatchFileStream(&originalPatchFile);
+    originalPatchFileStream << "A: 0x1CCC" << Common_Strings::STRING_NEW_LINE;
+    originalPatchFileStream << "V: 2529C0" << Common_Strings::STRING_NEW_LINE;
+    originalPatchFileStream.flush();
+    originalPatchFile.flush(); originalPatchFile.close();
+
+    //Scan for .hexp files to extract graphics from
     if (!dir.cd("Patches") || dir.isEmpty()) { qWarning() << "The patches directory is empty!"; return 1; }
     QStringList files = dir.entryList(QStringList("*.hexp"), QDir::Files | QDir::NoDotAndDotDot);
     if (files.isEmpty()) { qWarning() << "The patches directory does not contain any .hexp files!"; return 1; }
