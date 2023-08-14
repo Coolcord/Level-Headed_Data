6c83511b3c03594e5a92a06f9bb03b1b7ff851fc
Coolcord <coolcord24@tutanota.com>
2023-08-14 00:12:26 -0700

    Dropped Qt5 from CMakeLists.txt

--- Tools/Graphics_Ripper/Build_Scripts/Windows/Prepare_Runtime.bat
+++ Tools/Graphics_Ripper/Build_Scripts/Windows/Prepare_Runtime.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 COLOR 0A
 
-SET QT=Qt_5_12_2_MinGW_32_bit
+SET QT=Qt_6_5_2_MinGW_64_bit
 SET MODE=%1
 IF %1.==. SET MODE=Debug
 SET SOURCE_LOCATION=F:\Documents\Source_Code
@@ -10,7 +10,7 @@ REM Prepare Folders
 MKDIR "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\%MODE%\Plugins" >NUL 2>&1
 
 REM Delete Old Plugins
-ERASE "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\%MODE%\Plugins\Hexagon.dll" >NUL 2>&1
+ERASE "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\Plugins\Hexagon.dll" >NUL 2>&1
 
 REM Install New Plugins
-COPY "%SOURCE_LOCATION%\Hexagon\build-Hexagon-Desktop_%QT%-%MODE%\%MODE%\Hexagon.dll" "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\%MODE%\Plugins\Hexagon.dll"
+COPY "%SOURCE_LOCATION%\Hexagon\build-Hexagon-Desktop_%QT%-%MODE%\libHexagon.dll" "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\Plugins\Hexagon.dll"
