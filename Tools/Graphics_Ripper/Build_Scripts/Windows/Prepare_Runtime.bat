346c57ff8f8c59cca32b7a18338fa97c632d9158
Coolcord <coolcord24@tutanota.com>
2023-08-14 12:03:55 -0700

    Fixed the prepare script for Graphics_Ripper

--- Tools/Graphics_Ripper/Build_Scripts/Windows/Prepare_Runtime.bat
+++ Tools/Graphics_Ripper/Build_Scripts/Windows/Prepare_Runtime.bat
@@ -4,10 +4,10 @@ COLOR 0A
 SET QT=Qt_6_5_2_MinGW_64_bit
 SET MODE=%1
 IF %1.==. SET MODE=Debug
-SET SOURCE_LOCATION=F:\Documents\Source_Code
+SET SOURCE_LOCATION=D:\Documents\Source_Code
 
 REM Prepare Folders
-MKDIR "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\%MODE%\Plugins" >NUL 2>&1
+MKDIR "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\Plugins" >NUL 2>&1
 
 REM Delete Old Plugins
 ERASE "%SOURCE_LOCATION%\Level-Headed_Data\Tools\build-Graphics_Ripper-Desktop_%QT%-%MODE%\Plugins\Hexagon.dll" >NUL 2>&1
